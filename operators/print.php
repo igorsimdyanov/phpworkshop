4635dd58226aef391356b9c296c452b8e2fcafad
Anatolij <developer@languages-with-music.com>
2019-04-23 12:22:02 +0300

    add <br />

--- operators/print.php
+++ operators/print.php
@@ -1,5 +1,5 @@
 <?php
 $number = 216;
-print('Сегодня ' . $number . ' участников'); // Сегодня 216 участников
-echo print('Hello, world!');    // Hello world!1
+print('Сегодня ' . $number . ' участников' . '<br />); // Сегодня 216 участников
+echo print('Hello, world!');    // Hello world!
 // print echo "Hello, world!";  // Ошибка
